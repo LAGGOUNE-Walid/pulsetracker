9152f4065e3e97b30e7b5a5c606aae57e4575803
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2024-10-06 12:15:59 +0100

    updated database build

--- mariadbBuild/disable-thp.sh
+++ mariadbBuild/disable-thp.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-if [ -f /sys/kernel/mm/transparent_hugepage/enabled ]; then
-    echo "Disabling Transparent Huge Pages (THP)"
-    echo never > /sys/kernel/mm/transparent_hugepage/enabled
-    echo never > /sys/kernel/mm/transparent_hugepage/defrag
-else
-    echo "THP settings file not found. Skipping THP disable step."
-fi
+set -e
 
-# Start MariaDB
-exec "$@"
+echo never | tee /sys/kernel/mm/transparent_hugepage/enabled
+echo never | tee /sys/kernel/mm/transparent_hugepage/defrag
