20f8855dae1f7595f8edf31c779a751032bf05a3
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2024-10-08 17:31:44 +0100

    added database seeder

--- src/database/seeders/DeviceTypeSeeder.php
+++ src/database/seeders/DeviceTypeSeeder.php
@@ -13,5 +13,19 @@ class DeviceTypeSeeder extends Seeder
     public function run(): void
     {
         // DeviceType::factory()->count(20)->create();
+        DeviceType::create(['name' => 'Smartphone', 'svg' => '']);
+        DeviceType::create(['name' => 'Laptop', 'svg' => '']);
+        DeviceType::create(['name' => 'Car', 'svg' => '']);
+        DeviceType::create(['name' => 'Bus', 'svg' => '']);
+        DeviceType::create(['name' => 'Bicycle', 'svg' => '']);
+        DeviceType::create(['name' => 'Drone', 'svg' => '']);
+        DeviceType::create(['name' => 'Robot', 'svg' => '']);
+        DeviceType::create(['name' => 'Smartwatch', 'svg' => '']);
+        DeviceType::create(['name' => 'Fleet vehicle', 'svg' => '']);
+        DeviceType::create(['name' => 'Boat', 'svg' => '']);
+        DeviceType::create(['name' => 'IoT Sensor', 'svg' => '']);
+        DeviceType::create(['name' => 'Personal GPS tracker', 'svg' => '']);
+        DeviceType::create(['name' => 'Pet Tracker', 'svg' => '']);
+        DeviceType::create(['name' => 'Agriculture Equipment', 'svg' => '']);
     }
 }
