5366eee232029ba68be47e118b987d8691654d18
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2024-10-12 18:24:51 +0100

    updated terms

--- src/resources/views/terms-of-use.blade.php
+++ src/resources/views/terms-of-use.blade.php
@@ -67,54 +67,93 @@
                 <div class="row g-0">
                     <div class="col-md-12">
                         <div class="text-white p-4 p-md-5">
-                            <h1>PulseTracker Terms of Service</h1>
-                            <small>Last Updated: October 9, 2024</small>
+                            <h1 class="mt-5">PulseTracker Terms of Service</h1>
+                            <small>Last Updated: October 12, 2024</small>
 
                             <p>Welcome to PulseTracker! By using our service, you agree to comply with the following
                                 terms and conditions. If you do not agree, you should not use PulseTracker.</p>
 
-                            <h1>1. Account Registration</h1>
+                            <h1 class="mt-5">1. Account Registration</h1>
                             <p>Anyone, whether an individual or a company, can create an account. Users must provide an
                                 email address and password, and are responsible for safeguarding their credentials.</p>
 
-                            <h1>2. Subscription Plans and Payment</h1>
-                            <p>PulseTracker offers Free, Pro, and Enterprise plans, billed monthly through Paddle. In
-                                most cases
-                                , payments for Pulsetracker subscriptions and services aren't refundable, If you have an
-                                issue with your account, or think there has been an error in billing, please contact us
-                                by mail <a href="mailto:contact@pulsestracker.com">contact@pulsestracker.com</a>. If you
-                                upgrade or downgrade your plan, an
-                                immediate charge will occur, but your quota max limits will only change, not reset.
-                                Quotas reset at
-                                the beginning of each billing period. Canceled subscriptions remain active until the end
-                                of the billing period. Payment methods include Visa, Mastercard, Apple pay, and PayPal.</p>
-
-                            <h1>3. Data Privacy and Security</h1>
+                            <h1 class="mt-5">2. Subscription Plans and Payment</h1>
+                            <p>PulseTracker offers Free, Pro, and Enterprise plans, billed monthly through Paddle.
+                                <br />
+                                If you upgrade or downgrade your plan, an immediate charge will occur, but your quota
+                                max limits will only change, not reset.
+                                <br />
+                                Quotas reset at the beginning of each billing period. Canceled subscriptions remain
+                                active until the end
+                                of the billing period. Payment methods include Visa, Mastercard, Apple pay, and PayPal.
+                            </p>
+                            <h1 class="mt-5">3.Refunds</h1>
+                            <p>
+                                At PulseTracker, we strive to deliver a high-quality service that meets your needs.
+                                Given the nature of our real-time tracking and subscription-based service, we do not
+                                offer refunds for any subscription payments.
+                                <br />
+                                <br />
+                                This includes cases where a user consumes their account quota early or within a short
+                                period of time. Once a subscription is initiated, it will remain active until the end of
+                                the billing period, but no refunds will be processed under any circumstances.
+                                <br />
+                                <br />
+                                In the event of billing errors, please contact our support team at <a
+                                    href="mailto:contact@pulsestracker.com">contact@pulsestracker.com</a>. We will
+                                review the issue and make any necessary corrections.
+                            </p>
+                            <h1 class="mt-5">4.Fee Changes</h1>
+                            <p>
+                                Pulsetracker, in its sole discretion and at any time, may modify Subscription
+                                fees for the Subscriptions. Any Subscription fee change will become effective at the end
+                                of the then-current Billing Cycle.
+                                <br />
+                                <br />
+                                Pulsetracker, will provide you with a reasonable prior notice of any change in
+                                Subscription fees to give you an opportunity to terminate your Subscription before such
+                                change becomes effective.
+                                <br />
+                                <br />
+                                Your continued use of Service after Subscription fee change comes into effect
+                                constitutes your agreement to pay the modified Subscription fee amount.
+                            </p>
+                            <h1 class="mt-5">3. Data Privacy and Security</h1>
                             <p>PulseTracker collects and stores user data, including email address, password, and name,
                                 and device data such as IP address, latitude, longitude, app ID, client ID, and any
                                 additional data specified by the developer using the service.
+                                <br />
+                                <br />
                                 We use data hosting service providers in the EU to host the information we collect, and
-                                we use technical measures to secure your data. While we implement safeguards designed to
+                                we use technical measures to secure your data.
+                                <br />
+                                <br />
+                                While we implement safeguards designed to
                                 protect your information, no security system is impenetrable and due to the inherent
                                 nature of the Internet, we cannot guarantee that data, during transmission through the
                                 Internet or while stored on our systems or otherwise in our care, is absolutely safe
                                 from intrusion by others. We will respond to requests about this within a reasonable
                                 timeframe.
-
+                                <br />
+                                <br />
                                 Sensitive and private data exchange for our Services happen over an SSL secured
                                 communication channel and is encrypted and protected with digital signatures.
-
-                                We never store passwords in our database; they are always encrypted and hashed with
-                                individual salts. PulseTracker does not share user data with any third parties unless
+                                <br />
+                                <br />
+                                We never store passwords in our database; they are always encrypted and hashed .
+                                <br />
+                                <br />
+                                PulseTracker does not share user data with any third parties unless
                                 required by law in cases where users or their devices are linked to harmful activities
                                 such as terrorism or criminal behavior.
-                                <br/>
-                                <br/>
+                                <br />
+                                <br />
                                 PulseTracker uses Umami, an open-source analytics service, to collect anonymous usage
                                 data to improve the service. For more information on Umami's privacy policy, please
-                                visit <a href="https://umami.is/privacy">Umami's privacy policy</a>.</p>
+                                visit <a href="https://umami.is/privacy">Umami's privacy policy</a>.
+                            </p>
 
-                            <h1>4. User Responsibilities</h1>
+                            <h1 class="mt-5">4. User Responsibilities</h1>
                             <p>Users must use PulseTracker in compliance with all applicable laws and regulations. You
                                 are responsible for any activity that occurs under your account, including ensuring that
                                 the tracked devices belong to you or that you have permission to track them. Users must
@@ -123,38 +162,45 @@
                                 the GPS data collected from devices and acknowledge that PulseTracker does not guarantee
                                 the accuracy of this data.</p>
 
-                            <h1>5. Limitation of Liability</h1>
+                            <h1 class="mt-5">5. Limitation of Liability</h1>
                             <p>PulseTracker provides real-time location tracking and data storage, broadcasting services
-                                as a backend, but we do not guarantee uninterrupted or error-free service. PulseTracker
-                                is not liable for any inaccuracies in the location data or for any harm or loss
-                                resulting from service outages, data delays, or unauthorized access. PulseTracker
-                                disclaims all warranties of any kind, whether express or implied.</p>
+                                as a backend, but we do not guarantee uninterrupted or error-free service.
+                                <br />
+                                <br />
+                                PulseTracker is not liable for any inaccuracies in the location data or for any harm or
+                                loss
+                                resulting from service outages, data delays, or unauthorized access.
+                                <br />
+                                <br />
+                                PulseTracker
+                                disclaims all warranties of any kind, whether express or implied.
+                            </p>
 
-                            <h1>6. Intellectual Property</h1>
+                            <h1 class="mt-5">6. Intellectual Property</h1>
                             <p>All PulseTracker software, branding, and technology are the intellectual property of
                                 PulseTracker and its licensors. You are granted a limited, non-transferable license to
                                 use the service under these terms.</p>
 
-                            <h1>7. Account Termination</h1>
+                            <h1 class="mt-5">7. Account Termination</h1>
                             <p>We may terminate or suspend your account with 5 days' notice via email if you violate
                                 these terms. Reasons for termination include, but are not limited to, non-payment,
                                 misuse of the service, or use linked to criminal activity.</p>
 
-                            <h1>8. Governing Law</h1>
+                            <h1 class="mt-5">8. Governing Law</h1>
                             <p>These terms are governed by the laws of Europe, where PulseTracker servers are hosted. If
                                 you reside outside Europe, local laws may also apply.</p>
 
-                            <h1>9. Dispute Resolution</h1>
+                            <h1 class="mt-5">9. Dispute Resolution</h1>
                             <p>Any disputes arising from the use of PulseTracker will be resolved through binding
                                 arbitration in accordance with Europe law. Users waive the right to participate in
                                 class-action lawsuits.</p>
 
-                            <h1>10. Changes to the Terms</h1>
+                            <h1 class="mt-5">10. Changes to the Terms</h1>
                             <p>PulseTracker may update these terms at any time, with changes taking effect immediately
                                 upon posting to the website. Continued use of the service after updates constitutes
                                 acceptance.</p>
 
-                            <h1>11. Contact Information</h1>
+                            <h1 class="mt-5">11. Contact Information</h1>
                             <p>For questions or concerns, please contact us at <strong><a
                                         href="mailto:contact@pulsestracker.com">contact@pulsestracker.com</a></strong>.
                             </p>
