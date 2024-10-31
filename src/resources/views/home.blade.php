8c1de5f0b8b0fe9754495cb43313ede59f22313d
LAGGOUNE Walid <walidlaggoune159@gmail.com>
2024-10-31 19:25:19 +0100

    updated docs

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -233,7 +233,7 @@
                                     <path
                                         d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05">
                                     </path>
-                                </svg><strong>&nbsp;{{ $subscriptions['free']['size']['wesockets_limits'] }} websockets
+                                </svg><strong>&nbsp;{{ $subscriptions['enterprise']['size']['wesockets_limits'] ?? 'Unlimited' }} websockets
                                     connections</strong></li>
                             <li><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em"
                                     fill="currentColor" viewBox="0 0 16 16" class="bi bi-check-circle"
